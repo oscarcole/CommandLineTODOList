6daa2a0afd77fc8a06f2a63ec5a3b2ac45f25317
oscarcole <33353992+oscarcole@users.noreply.github.com>
2025-02-02 15:55:08 -0700

    Added in formatting for table, started working on Record class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-import requests
+from tabulate import tabulate
 
 # manages the loading, saving, and posting all records
 class DataManager:
@@ -12,6 +12,14 @@ class DataManager:
     def load_data(self):
         with open(self.file_path,'r') as file:
             return json.load(file)
+
+
+    def format_todos(self):
+        formatted_data = [
+            [todo["id"], "[X]" if todo["completed"] else "[ ]", todo["task"]]
+            for todo in self.data  # Now it uses self.data directly
+        ]
+        return tabulate(formatted_data, headers=["ID", "Status", "Task"], tablefmt="rounded_grid")
 
 
     # saves json file
@@ -29,8 +37,10 @@ class DataManager:
 
 
 class Record:
-    def create_new(self):
-        pass
+    @staticmethod
+    def create_new(id_number, todo_task, mark_completed):
+        cn_data_manager = DataManager()
+        cn_data_manager.save_data(id_number, todo_task, mark_completed)
 
     def edit_record(self):
         pass
@@ -38,6 +48,6 @@ class Record:
     def delete_record(self):
         pass
 
+
 data_manager = DataManager()
-data_manager.save_data(5, "Put up cord", "false")
-data_manager.load_data()
+print(data_manager.format_todos())
